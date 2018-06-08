1e7c4645cc82690dbdbddc688290546e4565481c
Charles Trout <charles.trout@gmail.com>
2018-06-07 20:07:45 -0400

    comments

--- game/dice.ts
+++ game/dice.ts
@@ -1,7 +1,33 @@
 const gridSize = 50;
 const dieSize = 100;
 
+//coordinates of each die
+const diePositions = [
+    {x: 50, y: 50},
+    {x: 200, y: 50},
+    {x: 350, y: 50},
+    {x: 50, y: 200},
+    {x: 200, y: 200},
+    {x: 350, y: 200}
+];
 
+//coordinates of each pip
+var pipPositions = [
+    {x: 50, y: 50},
+    {x: 20, y: 20},
+    {x: 20, y: 50},
+    {x: 20, y: 80},
+    {x: 80, y: 20},
+    {x: 80, y: 50},
+    {x: 80, y: 80},
+];
+
+//return an array of correct pips for the score
+function pips(...indexes: number[]): createjs.Shape[] {
+    return indexes.map(i => drawPip(pipPositions[i].x, pipPositions[i].y));
+}
+
+//draw a single die
 export function drawDie(x: number, y: number, score: number): createjs.Container {
     var result = new createjs.Container();
     result.x = x;
@@ -13,66 +39,40 @@ export function drawDie(x: number, y: number, score: number): createjs.Container
     return result;
 }
 
-
-
+//draw all diece
 export function drawDice(): createjs.Container[]
 {
-    //all 6 dice positions
-    var positions = [
-        {x: 50, y: 50},
-        {x: 200, y: 50},
-        {x: 350, y: 50},
-        {x: 50, y: 200},
-        {x: 200, y: 200},
-        {x: 350, y: 200}
-    ];
-        
     //randomize die values
-    var scores = Array.from(randomDice());
+    var scores = Array.from(randomDice(diePositions.length));
 
     //create random die in each position
-    return scores.map((score,i) => drawDie(positions[i].x, positions[i].y, score));
+    return scores.map((score,i) => drawDie(diePositions[i].x, diePositions[i].y, score));
 }
 
-function* randomDice(){
+//generate some random dice values
+function* randomDice(count: number){
     var i = 0;
-    while(i<6) {
+    while(i<count) {
         yield Math.floor((Math.random() * 6) + 1);
         i++;
     }
 }
 
+//draw the correct pips by location in the array for the score
 function drawScore(score: number): createjs.Shape[] 
 {
-    var pips = drawPips();
-
-    //return elements from an array by index
-    const pos = (...indexes: number[]) => indexes.map(i => pips[i]);
-    
     switch(score){
-        case 1: return pos(0);
-        case 2: return pos(1,6);
-        case 3: return pos(1,0,6);
-        case 4: return pos(1,3,4,6);
-        case 5: return pos(1,3,4,6,0);
-        case 6: return pos(1,2,3,4,5,6);
+        case 1: return pips(0);
+        case 2: return pips(1,6);
+        case 3: return pips(1,0,6);
+        case 4: return pips(1,3,4,6);
+        case 5: return pips(1,3,4,6,0);
+        case 6: return pips(1,2,3,4,5,6);
     }
 }
 
-function drawPips(): createjs.Shape[] 
-{
-    return [
-        drawPip(50, 50),
-        drawPip(20, 20),
-        drawPip(20, 50),
-        drawPip(20, 80),
-        drawPip(80, 20),
-        drawPip(80, 50),
-        drawPip(80, 80),
-    ]
-}
-
-function drawPip(x: number, y: number) {
+//draw a pip
+function drawPip(x: number, y: number): createjs.Shape {
     var g = new createjs.Graphics();
     g.setStrokeStyle(1);
     g.beginStroke("#999999");
@@ -86,6 +86,7 @@ function drawPip(x: number, y: number) {
     return shape;
 }
 
+//draw a die cube
 function drawCube(x: number, y: number) {
    
     var g = new createjs.Graphics();
